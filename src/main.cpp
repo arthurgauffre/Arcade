2c4609c190afbf1731e7b22f300a2b19357d3f77
OxiiLB <romain.giraud@epitech.eu>
2024-03-19 16:28:45 +0100

    [ADD] CMake

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+/*
+** EPITECH PROJECT, 2024
+** main.cpp
+** File description:
+** main.cpp
+*/
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, world!" << std::endl;
+    return 0;
+}
