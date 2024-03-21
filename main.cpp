d865755612264cb9c097e0595a8f8868ba8a22d0
arthurgauffre <arth.gauffre@gmail.com>
2024-03-21 09:46:31 +0100

    [ADD] custom command to copy executable to source directory

--- main.cpp
+++ main.cpp
@@ -9,6 +9,10 @@
 
 int main(int const argc, char const *const *argv)
 {
+    if (argc != 2) {
+        std::cerr << "Usage: " << argv[0] << " <library_path>" << std::endl;
+        return 84;
+    }
     std::cout << "Hello World" << std::endl;
     return 0;
 }
