43f8c49d960017a09d2eec8eb641b1915d2afee2
arthurgauffre <arth.gauffre@gmail.com>
2024-03-22 11:20:05 +0100

    [UPD] CMakeLists.txt and fix SDL2 initialization in Sdl2.cpp

--- lib/graphics/sdl2/Sdl2.cpp
+++ lib/graphics/sdl2/Sdl2.cpp
@@ -70,32 +70,32 @@ const arcade::IModule::LibName arcade::Sdl2::getName() const
 std::string *arcade::Sdl2::displayMenu()
 {
     std::string *menu = {NULL};
-    // if (SDL_Init(SDL_INIT_VIDEO) < 0) {
-    //     std::cerr << "SDL initialization failed: " << SDL_GetError() << std::endl;
-    //     return nullptr;
-    // }
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        std::cerr << "SDL initialization failed: " << SDL_GetError() << std::endl;
+        return nullptr;
+    }
 
-    // // Create a window
-    // SDL_Window *window = SDL_CreateWindow("Arcade Menu", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1920, 1080, SDL_WINDOW_SHOWN);
-    // if (!window) {
-    //     std::cerr << "Failed to create window: " << SDL_GetError() << std::endl;
-    //     SDL_Quit();
-    //     return nullptr;
-    // }
+    // Create a window
+    SDL_Window *window = SDL_CreateWindow("Arcade Menu", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1920, 1080, SDL_WINDOW_SHOWN);
+    if (!window) {
+        std::cerr << "Failed to create window: " << SDL_GetError() << std::endl;
+        SDL_Quit();
+        return nullptr;
+    }
 
-    // // Main loop
-    // bool quit = false;
-    // while (!quit) {
-    //     SDL_Event event;
-    //     while (SDL_PollEvent(&event)) {
-    //         if (event.type == SDL_QUIT) {
-    //             quit = true;
-    //         }
-    //     }
-    //     SDL_Delay(16);
-    // }
-    // SDL_DestroyWindow(window);
-    // SDL_Quit();
+    // Main loop
+    bool quit = false;
+    while (!quit) {
+        SDL_Event event;
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT) {
+                quit = true;
+            }
+        }
+        SDL_Delay(16);
+    }
+    SDL_DestroyWindow(window);
+    SDL_Quit();
     printf("SDL2 menu\n");
     return menu;
 }
