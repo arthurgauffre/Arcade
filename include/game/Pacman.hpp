9c8fd9e10f9cd26c6d76e41a74f4c6bbbfc3ebeb
OxiiLB <romain.giraud@epitech.eu>
2024-03-28 11:27:51 +0100

    [ADD] new architecture and beginning of pacman

--- include/game/Pacman.hpp
+++ include/game/Pacman.hpp
@@ -17,9 +17,18 @@ public:
   ~Pacman();
   void init();
   void updateGame();
-  void handdleKeyEvents(arcade::KeyboardInput key){};
+  std::pair<int, int> getPacman() const;
+  void setPacman(std::pair<int, int> snake);
+  std::vector<std::pair<int, int>> getGhosts() const;
+  void setGhosts(std::vector<std::pair<int, int>> ghosts);
+  std::vector<std::vector<int>> movePacman(std::vector<std::vector<int>> display_info);
+  void handdleKeyEvents(arcade::KeyboardInput key);
+  std::vector<std::vector<int>> moveGhosts(std::vector<std::vector<int>> display_info);
+
 protected:
 private:
+  std::pair<int, int> _pacman;
+  std::vector<std::pair<int, int>> _ghosts;
 };
 }; // namespace arcade
 
--- lib/games/pacman/Pacman.cpp
+++ lib/games/pacman/Pacman.cpp
@@ -23,6 +23,8 @@ void arcade::Pacman::init()
   data.sprite_value['C'] = "assets/default/item/item3.png";  // Coin
   data.sprite_value['P'] = "assets/default/npc/npc1.png";  // Pacman
 
+  std::pair<int, int> pacman = std::make_pair(10, 10);
+
   // Define the map
   data.display_info = {
       {'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W'},
@@ -35,7 +37,7 @@ void arcade::Pacman::init()
       {'W', 'M', 'M', 'M', 'M', 'M', 'M', 'M', 'M', 'M', 'M', 'M', 'M', 'M', 'M', 'M', 'M', 'M', 'M', 'M', 'W'},
       {'W', 'M', 'M', 'M', 'M', 'M', 'M', 'M', 'M', 'M', 'M', 'M', 'M', 'M', 'M', 'M', 'M', 'M', 'M', 'M', 'W'},
       {'W', 'M', 'M', 'M', 'M', 'M', 'M', 'M', 'M', 'M', 'M', 'M', 'M', 'M', 'M', 'M', 'M', 'M', 'M', 'M', 'W'},
-      {'W', 'M', 'M', 'M', 'M', 'M', 'M', 'P', 'P', 'P', 'P', 'M', 'M', 'M', 'M', 'M', 'M', 'M', 'M', 'M', 'W'},
+      {'W', 'M', 'M', 'M', 'M', 'M', 'M', 'M', 'M', 'M', 'M', 'M', 'M', 'M', 'M', 'M', 'M', 'M', 'M', 'M', 'W'},
       {'W', 'M', 'M', 'M', 'M', 'M', 'M', 'M', 'M', 'M', 'M', 'M', 'M', 'M', 'M', 'M', 'M', 'M', 'M', 'M', 'W'},
       {'W', 'M', 'M', 'M', 'M', 'M', 'M', 'M', 'M', 'M', 'M', 'M', 'M', 'M', 'M', 'M', 'M', 'M', 'M', 'M', 'W'},
       {'W', 'M', 'M', 'M', 'M', 'M', 'M', 'M', 'M', 'M', 'M', 'M', 'M', 'M', 'M', 'M', 'M', 'M', 'M', 'M', 'W'},
@@ -47,6 +49,10 @@ void arcade::Pacman::init()
       {'W', 'M', 'M', 'M', 'M', 'M', 'M', 'M', 'M', 'M', 'M', 'M', 'M', 'M', 'M', 'M', 'M', 'M', 'M', 'M', 'W'},
       {'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W'}
   };
+
+  data.display_info[pacman.first][pacman.second] = 'P';
+  this->setPacman(pacman);
+  this->setDirection(arcade::KeyboardInput::RIGHT);
 
   this->getCoreModule()->setGameData(data);
   return;
@@ -64,6 +70,15 @@ arcade::Pacman::~Pacman() {}
  */
 void arcade::Pacman::updateGame()
 {
+  arcade::GameData data = this->getCoreModule()->getGameData();
+  this->updateTimer();
+  if (this->getTimer().duration.count() >= 500) {
+    this->resetTimer();
+    // Update the game
+    data.display_info = this->movePacman(this->getCoreModule()->getGameData().display_info);
+    // data.display_info = this->moveGhosts(this->getCoreModule()->getGameData().display_info);
+  }
+  this->getCoreModule()->setGameData(data);
   return;
 }
 
@@ -84,4 +99,86 @@ extern "C" arcade::ModuleType getType()
 extern "C" std::string getName()
 {
   return "snake";
+}
+
+std::pair<int, int> arcade::Pacman::getPacman() const
+{
+  return this->_pacman;
+}
+
+void arcade::Pacman::setPacman(std::pair<int, int> pacman)
+{
+  this->_pacman = pacman;
+}
+
+std::vector<std::vector<int>> arcade::Pacman::movePacman(std::vector<std::vector<int>> display_info)
+{
+  std::pair<int, int> pacman = this->getPacman();
+  std::pair<int, int> newPacman = pacman;
+  arcade::KeyboardInput direction = this->getDirection();
+  // Move the snake
+  if (direction == arcade::KeyboardInput::UP)
+    newPacman.first--;
+  else if (direction == arcade::KeyboardInput::DOWN)
+    newPacman.first++;
+  else if (direction == arcade::KeyboardInput::LEFT)
+    newPacman.second--;
+  else if (direction == arcade::KeyboardInput::RIGHT)
+    newPacman.second++;
+
+  // Check if the snake is hitting a wall
+  if (display_info[newPacman.first][newPacman.second] == 'W') {
+    return display_info;
+  }
+  
+  // clear the map
+  if (pacman != newPacman)
+    display_info[pacman.first][pacman.second] = 'M';
+  else
+    display_info[pacman.first][pacman.second] = 'C';
+
+
+  // Update the map
+  display_info[newPacman.first][newPacman.second] = 'P';
+  this->setPacman(newPacman);
+  return display_info;
+}
+
+std::vector<std::pair<int, int>> arcade::Pacman::getGhosts() const
+{
+  return this->_ghosts;
+}
+
+void arcade::Pacman::setGhosts(std::vector<std::pair<int, int>> ghosts)
+{
+  this->_ghosts = ghosts;
+}
+
+std::vector<std::vector<int>> arcade::Pacman::moveGhosts(std::vector<std::vector<int>> display_info)
+{
+  return display_info;
+}
+
+void arcade::Pacman::handdleKeyEvents(arcade::KeyboardInput key)
+{
+  switch (key) {
+    case arcade::KeyboardInput::UP:
+      if (this->getDirection() != arcade::KeyboardInput::DOWN)
+        this->setDirection(arcade::KeyboardInput::UP);
+      break;
+    case arcade::KeyboardInput::DOWN:
+      if (this->getDirection() != arcade::KeyboardInput::UP)
+        this->setDirection(arcade::KeyboardInput::DOWN);
+      break;
+    case arcade::KeyboardInput::LEFT:
+      if (this->getDirection() != arcade::KeyboardInput::RIGHT)
+        this->setDirection(arcade::KeyboardInput::LEFT);
+      break;
+    case arcade::KeyboardInput::RIGHT:
+      if (this->getDirection() != arcade::KeyboardInput::LEFT)
+        this->setDirection(arcade::KeyboardInput::RIGHT);
+      break;
+    default:
+      break;
+  }
 }
