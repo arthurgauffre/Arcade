739a9d6f91c19542a82de8d81b61e9e07380f65e
arthurgauffre <arth.gauffre@gmail.com>
2024-04-02 15:52:37 +0200

    Add ICoreModule, IDisplayModule, and IGameModule interfaces

---
+++ interface/include/arcade/IDisplayModule.hpp
@@ -0,0 +1,42 @@
+/*
+** EPITECH PROJECT, 2024
+** bsArcade
+** File description:
+** IDisplayModule
+*/
+
+#ifndef IDISPLAYMODULE_HPP_
+#define IDISPLAYMODULE_HPP_
+
+#include <Arcade.hpp>
+#include <CoreModule.hpp>
+#include <iostream>
+
+namespace arcade {
+class CoreModule;
+class IDisplayModule {
+public:
+  IDisplayModule(){};
+  virtual ~IDisplayModule(){};
+
+  virtual std::string getName() const = 0;
+
+  void sendGameData(arcade::GameData data);
+  void sendMenuData(arcade::MenuData data);
+
+  virtual void setCoreModule(arcade::CoreModule *coreModule) = 0;
+  virtual arcade::CoreModule *getCoreModule() const = 0;
+
+  virtual void clearWindow() = 0;
+  virtual void displayWindow() = 0;
+  virtual arcade::KeyboardInput getInput() = 0;
+  virtual void drawSprite(std::pair<char, std::string> sprite, int x, int y, int width, int height, int rotation) = 0;
+  virtual void drawText(const std::string text, int x, int y, int size) = 0;
+
+protected:
+  arcade::CoreModule *_coreModule;
+  arcade::KeyboardInput _input;
+};
+}; // namespace arcade
+
+#endif /* !IDISPLAYMODULE_HPP_ */
